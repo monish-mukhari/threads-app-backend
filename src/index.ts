e47ce45a1ea351728f0f73a165add0e178ad2ae4
monish-mukhari <manishmukherjee91@gmail.com>
2024-07-14 18:47:14 +0530

    added prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 const PORT = process.env.PORT || 3000;
 
 
@@ -14,11 +15,29 @@ async function startServer() {
                 hello: String
                 say(name: String): String
             }
+
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => 'Hello world!',
                 say: (_, { name }: { name: string }) => `Hey, ${name}` 
+            },
+            Mutation: {
+                createUser: async (_, { firstName, lastName, email, password }: { firstName: string, lastName: string, email: string, password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt: "random_salt"
+                        }
+                    });
+                    return true;
+                }
             }
         },
     });
